c5071096652bdafb802354a896dd0efbafeab8a1
Jyn <53803817+Neung-ee@users.noreply.github.com>
2021-12-30 09:40:52 +0900

    ClientProvider created

---
+++ APProvider/src/main/resources/docs/create_table_review.sql
@@ -0,0 +1,11 @@
+create table review(
+    mb_id varchar2(20)
+        constraint review_mb_id_fk references member(mb_id),
+    SIGUN_NM varchar2(20),
+    BIZPLC_NM varchar2(100),
+    rv_score number(5),
+    rv_comment varchar(200)
+)
+
+commit;
+    
