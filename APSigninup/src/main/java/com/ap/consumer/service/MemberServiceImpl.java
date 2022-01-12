2de61fcc03fc92e4959ff91fdeb45facff1b24c8
Jyn <53803817+Neung-ee@users.noreply.github.com>
2022-01-12 14:00:45 +0900

    functions related to spring security created
    스프링 세큐리티 관련 소스 업로드
    해당 소스 적용시 로그인을 해야만 메인페이지로 넘어감
    회원생성 및 로그인 후 테스트 진행 하도록 할 것.
    
    ++마이페이지로 이동 및 추후 개발 편의성을 위해
    메인 화면에 menu를 추가하였습니다.
    success.html 과 main.css를 참고해 적용해주시면 감사하겠습니다.

--- APSigninup/src/main/java/com/ap/consumer/service/MemberServiceImpl.java
+++ APSigninup/src/main/java/com/ap/consumer/service/MemberServiceImpl.java
@@ -1,27 +1,97 @@
 package com.ap.consumer.service;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.authentication.InternalAuthenticationServiceException;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 import com.ap.consumer.dao.MemberDAO;
 import com.ap.consumer.vo.Member;
+import com.google.common.base.Optional;
 
 @Service
 public class MemberServiceImpl implements MemberService {
 	
 	@Autowired
 	private MemberDAO memberDAO;
-
+	
+	private BCryptPasswordEncoder passwordEncoder = new BCryptPasswordEncoder();
+	
+	//사용자 정보 조회
 	@Override
 	public Member findByMbid(String mb_id) {
-		// TODO Auto-generated method stub
-		return memberDAO.findByMbid(mb_id);
+		
+		Member member = new Member(mb_id, null, null, null, null);
+		member = memberDAO.findByMbid(mb_id);
+		
+		return member;
 	}
 
+	//회원가입
 	@Override
 	public void insertNewAccount(Member member) {
-		// TODO Auto-generated method stub
+		
+		member.setMb_pw(passwordEncoder.encode(member.getMb_pw()));
+		member.setMb_pw_confirm(passwordEncoder.encode(member.getMb_pw()));
+		
 		memberDAO.insertNewAccount(member);
 	}
 
+	//아이디 찾기
+	@Override
+	public String findIdByNameAndEmail(String mb_name, String mb_email) {
+		
+		String mb_id = "";
+		mb_id = memberDAO.findIdByNameAndEmail(mb_name, mb_email);
+		
+		return mb_id;
+	}
+	
+	//비밀번호 찾기
+	@Override
+	public int findPwByIdAndNameAndEmail(String mb_id, String mb_name, String mb_email) {
+		
+		int mb_pw = 0;
+		mb_pw = memberDAO.findPwByIdAndNameAndEmail(mb_id, mb_name, mb_email);
+		
+		return mb_pw;
+	}
+
+	//로그인
+	@Override
+	public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException, InternalAuthenticationServiceException {
+		
+		Member member = memberDAO.findByMbid(username);
+		
+		if(member == null) {
+			throw new UsernameNotFoundException(username + "is not exist");
+		}
+
+		List<GrantedAuthority> authorities = new ArrayList<>();
+		
+		authorities.add(new SimpleGrantedAuthority("ROLE_MEMBER"));
+		
+		return new User(member.getMb_id(), member.getMb_pw(), authorities);
+	}
+
+	//비밀번호 업데이트
+	@Override
+	public void updateNewPassword(String mb_id, String new_pw) {
+		
+		String encoded_new_pw = passwordEncoder.encode(new_pw);
+		
+		memberDAO.updateNewPassword(mb_id, encoded_new_pw);
+	}
+	
+	
+	
 }
