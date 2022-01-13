cbfc0c3ac5791491e49855f96b8e5af3d2bd5bb8
Jyn <53803817+Neung-ee@users.noreply.github.com>
2022-01-13 13:20:44 +0900

    AP User Detail Project created
    마이페이지 기능 프로젝트 생성

---
+++ APUserDetail/src/main/java/com/ap/user/ApUserDetailApplication.java
@@ -0,0 +1,13 @@
+package com.ap.user;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApUserDetailApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApUserDetailApplication.class, args);
+	}
+
+}
