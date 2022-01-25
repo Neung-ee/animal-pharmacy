306eeec0ac8fa6007bba172e9527b6be3548974d
Jyn <53803817+Neung-ee@users.noreply.github.com>
2022-01-25 09:24:25 +0900

    UserDetail edited

--- APUserDetail/src/main/java/com/ap/user/controller/MyPageController.java
+++ APUserDetail/src/main/java/com/ap/user/controller/MyPageController.java
@@ -6,7 +6,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 @Controller
 public class MyPageController {
 	
-	@GetMapping("/")
+	@GetMapping("/mypage")
 	public String mypage() {
 		return "mypage";
 	}
