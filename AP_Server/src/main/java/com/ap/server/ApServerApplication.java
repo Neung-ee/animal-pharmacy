149c57f1eb1365ce6aa7d33a77d2a397bebd8fef
keepadogexpert <kakaman0803@gmail.com>
2022-01-28 12:55:22 +0900

    ver2

---
+++ AP_Server/src/main/java/com/ap/server/ApServerApplication.java
@@ -0,0 +1,15 @@
+package com.ap.server;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+
+@SpringBootApplication
+@EnableEurekaServer
+public class ApServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApServerApplication.class, args);
+	}
+
+}
---
+++ AP_Server/src/test/java/com/ap/server/ApServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ap.server;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
